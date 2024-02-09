ff2ff5fb10a8ee60ab726ce7c390bd0c66ec6618
nebyouAz <nebazw@gmail.com>
2024-02-08 23:42:18 -0500

    BUG: Insert new test feature

--- analysis_packages/tests/test_data.py
+++ analysis_packages/tests/test_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 from analysis_packages.data import get_data
 
 def test_data():
     df = get_data()
     assert all(df.columns == ['Total', 'West', 'East'])
     assert isinstance(df.index, pd.DatetimeIndex)
+    assert len(np.unique(df.index.time) == 24)
